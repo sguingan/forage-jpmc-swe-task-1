3f2314a3feab709f0190fb280e6869fc03235c27
Beasttime <91510298+Beasttime@users.noreply.github.com>
2023-08-21 12:16:27 -0400

    Updated client3.py to fix stock price and ratio calculations

--- client3.py
+++ client3.py
@@ -26,34 +26,37 @@ import urllib.request
 QUERY = "http://localhost:8080/query?id={}"
 
 # 500 server request
-N = 500
-
+N = 5
 
 def getDataPoint(quote):
     """ Produce all the needed values to generate a datapoint """
-    """ ------------- Update this function ------------- """
     stock = quote['stock']
     bid_price = float(quote['top_bid']['price'])
     ask_price = float(quote['top_ask']['price'])
-    price = bid_price
+    price = (bid_price + ask_price) / 2  # Updated price calculation
     return stock, bid_price, ask_price, price
-
 
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
-    """ ------------- Update this function ------------- """
-    return 1
-
+    if price_b == 0:
+        # Avoid dividing by zero
+        return None
+    return price_a / price_b
 
 # Main
 if __name__ == "__main__":
     # Query the price once every N seconds.
+    prices = {}  # Dictionary to store stock prices
+
     for _ in iter(range(N)):
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
-
-        """ ----------- Update to get the ratio --------------- """
+        
         for quote in quotes:
             stock, bid_price, ask_price, price = getDataPoint(quote)
+            prices[stock] = price  # Store price in the prices dictionary
             print("Quoted %s at (bid:%s, ask:%s, price:%s)" % (stock, bid_price, ask_price, price))
 
-        print("Ratio %s" % getRatio(price, price))
+        # Assuming there are two stock prices
+        stock_a, stock_b = prices.keys()
+        ratio = getRatio(prices[stock_a], prices[stock_b])
+        print("Ratio %s" % ratio)
